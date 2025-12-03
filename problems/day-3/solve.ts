f28fac74fd2b8c5e512318578c53a1ad389e2fed
Mihai Toader <mihai.toader@sectorlabs.ro>
2025-12-03 18:39:43 +0200

    Day 3 first

---
+++ problems/day-3/solve.ts
@@ -0,0 +1,46 @@
+import IOManager from "../../lib/IOManager.ts";
+
+type InType = number[][];
+type OutType = string;
+
+class DataManager extends IOManager<InType, OutType> {
+    parseInputData(data: string): InType {
+        return data.split("\n").map((line) =>
+            line.split("").map((value) => parseInt(value))
+        );
+    }
+
+    parseOutputData(data: OutType): string {
+        return `${data}`;
+    }
+}
+
+async function solveFirst(version: string) {
+    const dataManager = new DataManager(3, version, "first");
+    const data = await dataManager.readData();
+
+    let maximumJoltage = 0;
+    data.forEach((battery) => {
+        let max;
+        for (let i = 0; i < battery.length - 1; i++) {
+            for (let j = i + 1; j < battery.length; j++) {
+                const joltage = battery[i] * 10 + battery[j];
+                if (!max || joltage > max) {
+                    max = joltage;
+                }
+            }
+        }
+        maximumJoltage += max;
+    });
+    console.log(maximumJoltage);
+    await dataManager.writeData(maximumJoltage);
+}
+
+async function solveSecond(version: string) {
+    const dataManager = new DataManager(3, version, "second");
+    const data = await dataManager.readData();
+
+    await dataManager.writeData("out");
+}
+
+export { solveFirst, solveSecond };
