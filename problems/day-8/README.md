87d85a5b1f44fc932eb1c3349189879f9b64dc45
Mihai Toader <mihai.toader@sectorlabs.ro>
2025-12-08 23:42:08 +0200

    Day 8 - first

---
+++ problems/day-8/README.md
@@ -0,0 +1,1 @@
+# [Day 8](https://adventofcode.com/2025/day/8)
---
+++ problems/day-8/solve.ts
@@ -0,0 +1,126 @@
+import IOManager from "../../lib/IOManager.ts";
+
+type Point = {
+    x: number;
+    y: number;
+    z: number;
+};
+type InType = Point[];
+type OutType = any;
+
+class DataManager extends IOManager<InType, OutType> {
+    parseInputData(data: string): InType {
+        return data.split("\n").map((line) => {
+            const [x, y, z] = line.split(",");
+            return {
+                x: parseInt(x),
+                y: parseInt(y),
+                z: parseInt(z),
+            };
+        });
+    }
+
+    parseOutputData(data: OutType): string {
+        return "";
+    }
+}
+
+function calculateDistance(first: Point, second: Point) {
+    return Math.sqrt(
+        Math.pow(first.x - second.x, 2) + Math.pow(first.y - second.y, 2) +
+            Math.pow(first.z - second.z, 2),
+    );
+}
+
+function createCircuit(
+    boxesCircuits: number[][],
+    first: number,
+    second: number,
+) {
+    const firstCircuitIndex = boxesCircuits.findIndex((circuit) =>
+        circuit.includes(first)
+    );
+    const secondCircuitIndex = boxesCircuits.findIndex((circuit) =>
+        circuit.includes(second)
+    );
+
+    const firstCircuit = boxesCircuits[firstCircuitIndex];
+    const secondCircuit = boxesCircuits[secondCircuitIndex];
+
+    if (!firstCircuit && !secondCircuit) {
+        boxesCircuits.push([first, second]);
+        return;
+    }
+
+    if (firstCircuit && secondCircuit) {
+        // they are in the same circuit
+        if (firstCircuit.includes(second) || secondCircuit.includes(first)) {
+            return;
+        }
+        // they are in different
+        boxesCircuits[firstCircuitIndex] = [...firstCircuit, ...secondCircuit];
+        boxesCircuits.splice(secondCircuitIndex, 1);
+    }
+
+    if (firstCircuit) {
+        firstCircuit.push(second);
+        return;
+    }
+
+    if (secondCircuit) {
+        secondCircuit.push(first);
+        return;
+    }
+}
+
+const CIRCUITS = 1000;
+
+async function solveFirst(version: string) {
+    const dataManager = new DataManager(8, version, "first");
+    const data = await dataManager.readData();
+
+    const boxesCircuits = [];
+    let currentMin = 0;
+    for (let circuits = 0; circuits < CIRCUITS; circuits++) {
+        let min = Infinity;
+        let firstPoint;
+        let secondPoint;
+        for (let i = 0; i < data.length - 1; i++) {
+            for (let j = i + 1; j < data.length; j++) {
+                const distance = calculateDistance(data[i], data[j]);
+                if (distance < min && distance > currentMin) {
+                    min = distance;
+                    firstPoint = i;
+                    secondPoint = j;
+                }
+            }
+        }
+        if (firstPoint === undefined || secondPoint === undefined) {
+            console.log("oh no");
+            return;
+        }
+        createCircuit(boxesCircuits, firstPoint, secondPoint);
+        currentMin = min;
+    }
+
+    // for (let i = 0; i < data.length; i++) {
+    //     const circuit = boxesCircuits.find(c => c.includes(i));
+    //     if (!circuit) {
+    //         boxesCircuits.push([i])
+    //     }
+    // }
+    boxesCircuits.sort((c1, c2) => c2.length - c1.length);
+
+    const res = boxesCircuits[0].length * boxesCircuits[1].length *
+        boxesCircuits[2].length;
+    console.log(res);
+}
+
+async function solveSecond(version: string) {
+    const dataManager = new DataManager(8, version, "second");
+    const data = await dataManager.readData();
+
+    console.log(data);
+}
+
+export { solveFirst, solveSecond };
